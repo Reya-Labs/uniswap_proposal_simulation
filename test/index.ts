5045dd632bdfbdcd05cf53680c73c8c1adcc347a
Artur Begyan <arturbegyan98@gmail.com>
2022-02-24 17:21:49 +0000

    voltz uni governance proposal

---
+++ test/index.ts
@@ -0,0 +1,188 @@
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+import { waffle } from "hardhat";
+import { BigNumber, Contract, Wallet } from 'ethers'
+import { GOVERNOR_BRAVO_ABI, TIMELOCK_ABI, ENS_REGISTRY_ABI, ENS_PUBLIC_RESOLVER_ABI, UNI_ABI } from "./utils";
+import { namehash } from '@ethersproject/hash';
+import { keccak256 } from '@ethersproject/keccak256';
+import { utils } from 'ethers';
+import { Interface } from '@ethersproject/abi'; 
+import "hardhat";
+
+const { provider } = waffle;
+
+describe("Uni proposal simulation", async () => {
+  let wallet: Wallet, other: Wallet;
+
+
+  async function advanceBlockHeight(blocks:number) {
+    const txns = [];
+    for (let i = 0; i < blocks; i++) {
+      txns.push(network.provider.send('evm_mine'));
+    }
+    await Promise.all(txns);
+  }
+
+  it("proposal simulation", async () => {
+    // get the governor bravo contract
+    const governorBravoAddress = "0x408ED6354d4973f66138C91495F2f2FCbd8724C3"
+    const governorBravo = new Contract(governorBravoAddress, GOVERNOR_BRAVO_ABI, provider);
+
+    // get the timelock contract
+    const timelockAddress = "0x1a9C8182C09F50C8318d769245beA52c32BE35BC";
+    const timeLock = new Contract(timelockAddress, TIMELOCK_ABI, provider );
+
+    // get signers
+    [wallet, other] = await (ethers as any).getSigners();
+    
+    // check the timelock from the governor matches the timelock address
+    const timelockAddressFromGovernor = await governorBravo.timelock()
+
+    expect(timelockAddressFromGovernor).to.eq(timeLock.address);
+
+    // wallet submits a proposal
+    
+    const NODE_TOP_LEVEL: string = namehash("uniswap.eth");
+    const LABEL: string = keccak256(utils.toUtf8Bytes("v3-core-license-grants"));
+    const OWNER_UNISWAP_GOVERNANCE_TIMELOCK: string = "0x1a9C8182C09F50C8318d769245beA52c32BE35BC";
+    const RESOLVER_PUBLIC_ENS_RESOLVER: string = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41";
+    const TTL: number = 0;
+
+    const NODE: string = namehash("v3-core-license-grants.uniswap.eth");
+    const KEY: string = "Voltz Uni v3 Additional Use Grant";
+    const VALUE: string = `
+    Voltz Labs Technology Limited (“Voltz”) is granted an additional use grant to allow the Voltz DAO to use the Uniswap V3 Core software code (which is made available to Voltz subject to license available at https://github.com/Uniswap/v3-core/blob/main/LICENSE (the “Uniswap Code”)).  	
+    As part of this additional use grant, the Voltz DAO receives a limited worldwide license to use the Uniswap Code for the purposes of:
+    creating, deploying and making available aspects of an interest rate swap automated market maker (the “IRS AMM”); 
+    to modify and update the IRS AMM over time; and 
+    deploy the IRS AMM and portions thereof as smart contracts on blockchain-based applications and protocols.  
+    The Voltz DAO is permitted to use subcontractors to do this work.  
+    This license is conditional on Voltz and the Voltz DAO complying with the terms of the Business Source License 1.1, made available at https://github.com/Uniswap/v3-core/blob/main/LICENSE.
+    `
+    const ensRegistryInterface = new Interface(ENS_REGISTRY_ABI);
+    const setSubnodeRecordCalldata = ensRegistryInterface.encodeFunctionData("setSubnodeRecord", [NODE_TOP_LEVEL, LABEL, OWNER_UNISWAP_GOVERNANCE_TIMELOCK, RESOLVER_PUBLIC_ENS_RESOLVER, TTL]);
+
+    const ensPublicResolverInterface = new Interface(ENS_PUBLIC_RESOLVER_ABI);
+    const setTextCalldata = ensPublicResolverInterface.encodeFunctionData("setText", [NODE, KEY, VALUE]);
+
+    const ENS_REGISTRY_ADDRESS = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e";
+    const PUBLIC_ENS_RESOLVER_ADDRESS: string = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41";
+
+    const targets = [ENS_REGISTRY_ADDRESS, PUBLIC_ENS_RESOLVER_ADDRESS];
+    const values = [0, 0];
+    const sigs = ['', ''];
+    const calldatas = [setSubnodeRecordCalldata, setTextCalldata];
+    const description = "Voltz Additional Use Grant";
+    
+    const ensPublicResolver = new Contract(PUBLIC_ENS_RESOLVER_ADDRESS, ENS_PUBLIC_RESOLVER_ABI, provider);
+    let licenseText = await ensPublicResolver.text(NODE, KEY)
+    expect(licenseText).to.eq("");
+    
+    const a16zAddress = "0x2B1Ad6184a6B0fac06bD225ed37C2AbC04415fF4"
+    // delegate votes from whales to the wallet
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [a16zAddress], // a16z
+    });
+
+    const a16zSigner = await ethers.getSigner(a16zAddress);
+
+    // get the Uni contract
+    const uniAddress = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+    const uni = new Contract(uniAddress, UNI_ABI, provider);
+
+    
+    let blockNumber = (await provider.getBlock("latest")).number;
+    console.log("blockNumber OLD", blockNumber);
+
+    // check the prior votes of a16z
+    let priorVotesA16Z = await uni.getPriorVotes(a16zAddress, blockNumber - 1)
+    console.log("priorVotesA16Z", priorVotesA16Z)
+
+    // transfer ether to a16z to execute the delegation transaction
+    await wallet.sendTransaction(
+      {
+        to: a16zAddress,
+        value: ethers.utils.parseEther("1")
+      }
+    )
+      
+    let currentProposalCount = await governorBravo.proposalCount(); // expect 10
+    console.log("currentProposalCount", currentProposalCount);
+    expect(currentProposalCount).to.eq(10);
+
+
+    // make the proposal
+    await governorBravo.connect(a16zSigner).propose(targets, values, sigs, calldatas, description);
+
+    currentProposalCount = await governorBravo.proposalCount();
+    expect(currentProposalCount).to.eq(11);
+    console.log("current number of proposals created: "+currentProposalCount);
+    let proposalInfo = await governorBravo.proposals(11);
+    console.log(proposalInfo);
+
+    await advanceBlockHeight(13141); // fast forward through review period
+
+    const uniWhaleAddresses = [
+      "0x2b1ad6184a6b0fac06bd225ed37c2abc04415ff4",
+      "0xe02457a1459b6c49469bf658d4fe345c636326bf",
+      "0x8e4ed221fa034245f14205f781e0b13c5bd6a42e",
+      "0x61c8d4e4be6477bb49791540ff297ef30eaa01c2",
+      "0xa2bf1b0a7e079767b4701b5a1d9d5700eb42d1d1",
+      "0xe7925d190aea9279400cd9a005e33ceb9389cc2b",
+      "0x7e4a8391c728fed9069b2962699ab416628b19fa",
+    ]
+
+    // start casting votes
+    for  (let i = 0; i < uniWhaleAddresses.length; i++) {
+      
+      const whaleAddress = uniWhaleAddresses[i]
+      
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [whaleAddress], // a16z
+      });
+
+      const whaleSigner = await ethers.getSigner(whaleAddress);
+
+      // send ether to the whale address
+
+      await wallet.sendTransaction(
+        {
+          to: whaleAddress,
+          value: ethers.utils.parseEther("1")
+        }
+      )
+      
+      await governorBravo.connect(whaleSigner).castVote(11,1)
+
+    }
+
+    await advanceBlockHeight(40320); // fast forward through voting period
+
+    await governorBravo.connect(a16zSigner).queue(11);
+
+    proposalInfo = await governorBravo.proposals(11);
+
+    console.log(proposalInfo);
+
+    await network.provider.request({
+      method: "evm_increaseTime",
+      params: [172800],
+    });
+
+    await advanceBlockHeight(1) //after changing the time mine one block
+
+    await governorBravo.connect(a16zSigner).execute(11);
+
+    proposalInfo = await governorBravo.proposals(11);
+
+    console.log(proposalInfo); //expect "executed"
+
+    // check ens records are correctly updated
+    licenseText = await ensPublicResolver.text(NODE, KEY)
+    expect(licenseText).to.eq(VALUE)
+    
+
+  })
+})
---
+++ test/utils.ts
@@ -0,0 +1,97 @@
+
+
+export const GOVERNOR_BRAVO_ABI = [
+  'event NewAdmin(address oldAdmin, address newAdmin)',
+  'event NewImplementation(address oldImplementation, address newImplementation)',
+  'event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin)',
+  'event ProposalCanceled(uint256 id)',
+  'event ProposalCreated(uint256 id, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)',
+  'event ProposalExecuted(uint256 id)',
+  'event ProposalQueued(uint256 id, uint256 eta)',
+  'event ProposalThresholdSet(uint256 oldProposalThreshold, uint256 newProposalThreshold)',
+  'event VoteCast(address indexed voter, uint256 proposalId, uint8 support, uint256 votes, string reason)',
+  'event VotingDelaySet(uint256 oldVotingDelay, uint256 newVotingDelay)',
+  'event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod)',
+  'function BALLOT_TYPEHASH() view returns (bytes32)',
+  'function DOMAIN_TYPEHASH() view returns (bytes32)',
+  'function MAX_PROPOSAL_THRESHOLD() view returns (uint256)',
+  'function MAX_VOTING_DELAY() view returns (uint256)',
+  'function MAX_VOTING_PERIOD() view returns (uint256)',
+  'function MIN_PROPOSAL_THRESHOLD() view returns (uint256)',
+  'function MIN_VOTING_DELAY() view returns (uint256)',
+  'function MIN_VOTING_PERIOD() view returns (uint256)',
+  'function _acceptAdmin()',
+  'function _initiate(uint256 proposalCount)',
+  'function _setPendingAdmin(address newPendingAdmin)',
+  'function _setProposalThreshold(uint256 newProposalThreshold)',
+  'function _setVotingDelay(uint256 newVotingDelay)',
+  'function _setVotingPeriod(uint256 newVotingPeriod)',
+  'function admin() view returns (address)',
+  'function cancel(uint256 proposalId)',
+  'function castVote(uint256 proposalId, uint8 support)',
+  'function castVoteBySig(uint256 proposalId, uint8 support, uint8 v, bytes32 r, bytes32 s)',
+  'function castVoteWithReason(uint256 proposalId, uint8 support, string reason)',
+  'function execute(uint256 proposalId) payable',
+  'function getActions(uint256 proposalId) view returns (address[] targets, uint256[] values, string[] signatures, bytes[] calldatas)',
+  'function getReceipt(uint256 proposalId, address voter) view returns (tuple(bool hasVoted, uint8 support, uint96 votes))',
+  'function implementation() view returns (address)',
+  'function initialProposalId() view returns (uint256)',
+  'function initialize(address timelock_, address uni_, uint256 votingPeriod_, uint256 votingDelay_, uint256 proposalThreshold_)',
+  'function latestProposalIds(address) view returns (uint256)',
+  'function name() view returns (string)',
+  'function pendingAdmin() view returns (address)',
+  'function proposalCount() view returns (uint256)',
+  'function proposalMaxOperations() view returns (uint256)',
+  'function proposalThreshold() view returns (uint256)',
+  'function proposals(uint256) view returns (uint256 id, address proposer, uint256 eta, uint256 startBlock, uint256 endBlock, uint256 forVotes, uint256 againstVotes, uint256 abstainVotes, bool canceled, bool executed)',
+  'function propose(address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, string description) returns (uint256)',
+  'function queue(uint256 proposalId)',
+  'function quorumVotes() view returns (uint256)',
+  'function state(uint256 proposalId) view returns (uint8)',
+  'function timelock() view returns (address)',
+  'function uni() view returns (address)',
+  'function votingDelay() view returns (uint256)',
+  'function votingPeriod() view returns (uint256)',
+]
+
+
+export const TIMELOCK_ABI = [
+    'function executeTransaction(address target, uint256 value, string signature, bytes data, uint256 eta) payable returns (bytes)',
+    'function acceptAdmin()',
+    'function pendingAdmin() view returns (address)',
+    'function queueTransaction(address target, uint256 value, string signature, bytes data, uint256 eta) returns (bytes32)',
+    'function setPendingAdmin(address pendingAdmin_)',
+    'function cancelTransaction(address target, uint256 value, string signature, bytes data, uint256 eta)',
+    'function delay() view returns (uint256)',
+    'function MAXIMUM_DELAY() view returns (uint256)',
+    'function MINIMUM_DELAY() view returns (uint256)',
+    'function GRACE_PERIOD() view returns (uint256)',
+    'function setDelay(uint256 delay_)',
+    'function queuedTransactions(bytes32) view returns (bool)',
+    'function admin() view returns (address)',
+    'constructor(address admin_, uint256 delay_)',
+    'event NewAdmin(address indexed newAdmin)',
+    'event NewPendingAdmin(address indexed newPendingAdmin)',
+    'event NewDelay(uint256 indexed newDelay)',
+    'event CancelTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta)',
+    'event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta)',
+    'event QueueTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta)',
+  ]
+  
+
+export const ENS_REGISTRY_ABI = [
+    'event NewOwner(bytes32 indexed node, bytes32 indexed label, address owner)',
+    'event Transfer(bytes32 indexed node, address owner)',
+    'event NewResolver(bytes32 indexed node, address resolver)',
+    'event NewTTL(bytes32 indexed node, uint64 ttl)',
+    'event ApprovalForAll(address indexed owner, address indexed operator, bool approved)',
+    'function setSubnodeRecord(bytes32 node, bytes32 label, address owner, address resolver, uint64 ttl) external',
+];
+
+export const ENS_PUBLIC_RESOLVER_ABI = [
+    'function setText(bytes32 node, string calldata key, string calldata value) external',
+    'function text(bytes32 node, string calldata key) external view returns (string memory)'
+];
+
+export const UNI_ABI = [{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"minter_","type":"address"},{"internalType":"uint256","name":"mintingAllowedAfter_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"address","name":"fromDelegate","type":"address"},{"indexed":true,"internalType":"address","name":"toDelegate","type":"address"}],"name":"DelegateChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegate","type":"address"},{"indexed":false,"internalType":"uint256","name":"previousBalance","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"newBalance","type":"uint256"}],"name":"DelegateVotesChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"minter","type":"address"},{"indexed":false,"internalType":"address","name":"newMinter","type":"address"}],"name":"MinterChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DELEGATION_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"DOMAIN_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint32","name":"","type":"uint32"}],"name":"checkpoints","outputs":[{"internalType":"uint32","name":"fromBlock","type":"uint32"},{"internalType":"uint96","name":"votes","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"delegatee","type":"address"}],"name":"delegate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateBySig","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"delegates","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"getCurrentVotes","outputs":[{"internalType":"uint96","name":"","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"getPriorVotes","outputs":[{"internalType":"uint96","name":"","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minimumTimeBetweenMints","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"mintCap","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mintingAllowedAfter","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"numCheckpoints","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"minter_","type":"address"}],"name":"setMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+
